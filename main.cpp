2bde357f3ff8e530da4a48c40ca48091954eaab7
José Ladislao Lainez Ortega <thedragonladis@gmail.com>
2013-06-13 15:03:43 +0200

    Code for SDL testing added

---
+++ main.cpp
@@ -0,0 +1,45 @@
+//
+//  main.cpp
+//  Poles
+//
+//  Created by Jose Ladislao Lainez Ortega on 6/13/13.
+//  Copyright (c) 2013 None. All rights reserved.
+//
+
+#include <iostream>
+#include <SDL.h>
+
+int main(int argc, char** argv)
+{
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+    
+	// Initializes SDL
+	SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO);
+    
+	// Creates the Window we will use to render
+	window = SDL_CreateWindow("Magnet",
+                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              512,512,
+                              SDL_WINDOW_SHOWN);
+    
+	// Creates a new rendering context to draw in and binds it to the window created
+	renderer = SDL_CreateRenderer(window,-1,0);
+    
+	// Sets the drawing color for the renderer
+	SDL_SetRenderDrawColor(renderer,255,0,0,255);
+    
+	// Clears the renderer
+	SDL_RenderClear(renderer);
+    
+	// Sawps the rendering context to our rendering context
+	SDL_RenderPresent(renderer);
+    
+	// Makes SDL wait 5 seconds
+	SDL_Delay(5000);
+    
+	// Frees SDL's resources before closing
+	SDL_Quit();
+	
+	return 0;
+}
