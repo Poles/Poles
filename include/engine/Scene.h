144a1848fb4cc528078ab5abab799d55a8921faf
José Ladislao Lainez Ortega <thedragonladis@gmail.com>
2013-09-23 17:39:10 +0200

    Implemented Scene abstract class.
    This class represents every Scene in the game. It contains a list of objects and Artemis systems, so each Scene is independent from the others.

---
+++ include/engine/Scene.h
@@ -0,0 +1,36 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+#include <string>
+#include "engine/EventListener.h"
+#include <Artemis/Artemis.h>
+#include "engine/GameObject.h"
+
+class Scene : public EventListener {
+public:
+    Scene(std::string name);
+
+    virtual ~Scene();
+
+    virtual void                        onStart();
+
+    virtual void                        onEnd();
+
+    virtual void                        onLoop();
+
+    virtual void                        onRender() = 0;
+
+    GameObject*                         createGameObject(std::string name);
+
+    void                                destroyGameObject(GameObject*& object);
+
+private:
+    std::string                         name;   // Name of the Scene
+
+public:
+    artemis::World*                     world;  // Artemis systems container
+    int                                 idCounter;
+
+};
+
+#endif // SCENE_H
---
+++ src/engine/Scene.cpp
@@ -0,0 +1,58 @@
+#include "engine/Scene.h"
+
+Scene::Scene(std::string name):
+    name(name)
+{
+    // Setup Artemis systems
+    world = new artemis::World();
+
+    this->idCounter = 0;
+}
+
+Scene::~Scene() {
+    delete this->world;
+}
+
+void Scene::onStart() {
+
+}
+
+void Scene::onEnd() {
+
+}
+
+void Scene::onLoop() {
+    world->loopStart();
+}
+
+void Scene::onRender() {
+
+}
+
+/**
+ * @brief Scene::createGameObject
+ * @param name
+ * @return
+ */
+GameObject* Scene::createGameObject(std::string name) {
+    GameObject* object;
+
+    artemis::Entity& objectEntity = world->createEntity();
+    object = new GameObject(name, objectEntity);
+
+    // TODO: Add to the Scene objects list
+
+    return object;
+}
+
+/**
+ * @brief Scene::destroyGameObject
+ * @param object
+ */
+void Scene::destroyGameObject(GameObject*& object) {
+    world->deleteEntity(object->entity);
+
+    delete object;
+
+    object = NULL;
+}
