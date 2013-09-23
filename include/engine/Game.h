35e5cb96e3ae190fdf2b34698d1e382e00b836cc
José Ladislao Lainez Ortega <thedragonladis@gmail.com>
2013-09-23 21:49:10 +0200

    Aadded FPS Management and Delta Time.

--- include/engine/Game.h
+++ include/engine/Game.h
@@ -4,6 +4,8 @@
 #include <string>
 #include "engine/Window.h"
 
+#define ENGINE_MAX_FPS 60
+
 class Game {
 public:
     Game(std::string name);
@@ -15,7 +17,42 @@ public:
     void                                start();
 
 private:
+
+    void                                mainLoop();
+
+    void                                update();
+
+    void                                render();
+
+    void                                handleEvents();
+
+    void                                updateDeltaTime();
+
+    void                                countFramesPerSecond();
+
+    void                                manageFramesPerSecond();
+
+protected:
     std::string                         name;
+    bool                                run;
+
+private:
+    // Delta time
+    Uint32                              currentTime;    // Measures the number of ticks since the start of SDL
+    Uint32                              oldTime;        // Number of ticks of the last update
+    float                               deltaTime;      // Delta Time
+
+    // Frames Per Second
+    Uint32                              fpsCurrentTime;
+    Uint32                              fpsOldTime;
+    unsigned int                        fps;            // Number of frames rendered every second
+    unsigned int                        fpsCounter;     // Current number of frames (inside this second)
+
+    // Frame Skip
+    Uint32                              frameSkipCurrentTime;
+    Uint32                              frameSkipOldTime;
+    unsigned int                        frameSkip;      // Frames to skip when the game is running slowly
+    Uint32                              timePerFrame;
 
     /* Static Functions */
 public:
--- src/engine/Game.cpp
+++ src/engine/Game.cpp
@@ -2,6 +2,7 @@
 #include <SDL2/SDL_ttf.h>
 
 #include <iostream>
+#include <cmath>
 
 /*------------------------------------*/
 /*      Static variables              */
@@ -13,9 +14,25 @@ Window* Game::window = NULL;
 Game::Game(std::string name) {
     this->name = name;
 
+    this->run = false;
+
     // Initialize SDL and TTF
     SDL_Init(SDL_INIT_EVERYTHING);
     TTF_Init();
+
+    this->currentTime = SDL_GetTicks();
+    this->oldTime = 0;
+    this->deltaTime = 1.0f;
+
+    this->fpsCurrentTime = SDL_GetTicks();
+    this->fpsOldTime = 0;
+    this->fps = 0;
+    this->fpsCounter = 0;
+
+    this->frameSkip = 0;
+    this->frameSkipCurrentTime = SDL_GetTicks();
+    this->frameSkipOldTime = 0;
+    this->timePerFrame = (Uint32) ((1000.0d / (double)ENGINE_MAX_FPS));
 }
 
 Game::~Game() {
@@ -23,22 +40,124 @@ Game::~Game() {
 }
 
 
+/**
+ * @brief Game::createWindow
+ * @param w
+ * @param h
+ * @param mode
+ */
 void Game::createWindow(Uint16 w, Uint16 h, WindowMode mode) {
     window = new Window(this->name, w, h, mode);
 }
 
+/**
+ * @brief Game::createWindow
+ */
 void Game::createWindow() {
     window = new Window(this->name);
 }
 
+/**
+ * @brief Game::start
+ */
 void Game::start() {
-    window->clear();
-    window->draw();
-
-    SDL_Delay(5000);
+    this->run = true;
+    mainLoop();
 }
 
+/**
+ * @brief Game::mainLoop
+ */
+void Game::mainLoop() {
+    while (run) {
+        updateDeltaTime();
 
+        // Setup artemis::world.loopStart and artemis::world.setDelta
+        // TODO: Waiting for SceneManager
+
+        handleEvents();
+        update();
+        render();
+
+        manageFramesPerSecond();
+        countFramesPerSecond();
+    }
+}
+
+/**
+ * @brief Game::handleEvents
+ */
+void Game::handleEvents() {
+
+}
+
+/**
+ * @brief Game::update
+ */
+void Game::update() {
+
+}
+
+/**
+ * @brief Game::render
+ */
+void Game::render() {
+    this->window->clear();
+
+    // Do rendering
+
+    this->window->draw();
+
+}
+
+/**
+ * @brief Game::updateDeltaTime
+ */
+void Game::updateDeltaTime() {
+    this->oldTime = this->currentTime;
+    this->currentTime = SDL_GetTicks();
+    this->deltaTime = (this->currentTime - this->oldTime) / 1000.0f;
+}
+
+/**
+ * @brief Game::countFramesPerSecond
+ */
+void Game::countFramesPerSecond() {
+    this->fpsCurrentTime = SDL_GetTicks();
+    Uint32 timeElapsed = this->fpsCurrentTime - this->fpsOldTime;
+
+    if (timeElapsed >= 1000) {
+        // Reset FPS counting
+        this->fpsOldTime = this->fpsCurrentTime;
+        this->fps = this->fpsCounter;
+        this->fpsCounter = 0;
+        std::cout << "Running @" << this->fps << " fps." << std::endl;
+    } else {
+        this->fpsCounter++;
+    }
+}
+
+/**
+ * @brief Game::manageFramesPerSecond
+ */
+void Game::manageFramesPerSecond() {
+    this->frameSkipOldTime = this->frameSkipCurrentTime;
+    this->frameSkipCurrentTime = SDL_GetTicks();    // Total time since the start of the game
+
+    int timeElapsed = (int)this->frameSkipCurrentTime - (int)this->frameSkipOldTime; // Time elapsed since the last frame
+
+    // We are running too fast so we need to make the game wait a bit
+    int timeToWait = (int)this->timePerFrame - (int)timeElapsed;
+    if (timeToWait > 0) {
+        SDL_Delay(timeToWait);
+    }
+    this->frameSkipCurrentTime = SDL_GetTicks();
+}
+
+/**
+ * @brief Game::writeToConsole
+ * @param text
+ */
 void Game::writeToConsole(std::string text) {
     std::cout << text << std::endl;
 }
